300f081306ec11d4e0e05e69b360a17e36ebbcd1
Kurokitu <foxiti.a@gmail.com>
2021-08-09 06:24:59 +0800

    -new- 新增cd命令（路径补全和返回上层目录暂未完成） 新增依赖列表

--- main.py
+++ main.py
@@ -16,16 +16,19 @@ import os
 import json
 from os import *
 from cmd import Cmd
-from platform import version
+from pyreadline import Readline
+from pathlib import Path
 from libs.time import *
 from libs.clear import *
 from libs.file import *
 
 version = 1.0
 config = None
+readline = Readline()
 
 class VTMShell(Cmd):
-    prompt = '\033[0;32;40mVTMShell> \033[0m'
+    dir = Path.home()
+    prompt = '\033[0;32;40mVTMShell>\033[0m '
     intro = """
     \033[0;32m
      _    __________  ________ __         ____
@@ -42,19 +45,19 @@ class VTMShell(Cmd):
     \033[0m
     """ % (version, nowtime())
 
-    def do_exit(self, inp):
+    def do_exit(self, arg):
         'exit: exit the application. Shorthand: x q Ctrl-D.'
         print("Bye")
         return True
 
-    def do_time(self, inp):
+    def do_time(self, arg):
         'time: Get current system time'
         print("Time: ", nowtime())
 
     def do_dir(self, arg):
         'die: syntax: dir path -- displaya list of files and directories'
         if not arg:
-            print("\n".join(os.listdir(".")))
+            print("\n".join(os.listdir(self.dir)))
         elif os.path.exists(arg):
             print("\n".join(os.listdir(arg)))
         else:
@@ -64,15 +67,26 @@ class VTMShell(Cmd):
         'ls: syntax: dir path -- displaya list of files and directories'
         self.do_dir(arg)
 
-    def do_clear(self, inp):
+    def do_clear(self, arg):
         'clear: Clear Screen'
         clear()
+
+    def do_cd(self, arg):
+        'Working directory switching'
+        self.dir = arg
+
+    def do_pwd(self, arg):
+        'Get current path'
+        print(self.dir)
+
+    def do_testargs(self, arg):
+        print("Args:", arg)
 
     def default(self, inp):
         if inp == 'x' or inp == 'q':
             return self.do_exit(inp)
 
-        print("Default: {}".format(inp))
+        print("Unknown command: {}\nPlease refer to the help list.".format(inp))
 
 
 if __name__ == '__main__':
